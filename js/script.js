000e3387e5f1498a802d09e4da4f278c07f8cb16
Felipe Souza <souza.felipe87@gmail.com>
2026-03-13 17:36:19 -0300

    refactor: apply BEM methodology and SMACSS state rules to home and destination pages

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const menuNav = document.querySelector(".menu-nav-container");
-const btnCloseMenu = document.querySelector(".btn-close-menu");
+const headerToggle = document.querySelector(".header__toggle");
+const menuNav = document.querySelector(".main-nav");
+const btnCloseMenu = document.querySelector(".main-nav__close");
 
 // hamburger menu icon mobile behavior
 function openMenu() {
     menuNav.classList.add("is-open");
-    hamburgerMenu.setAttribute("aria-expanded", "true");
+    headerToggle.setAttribute("aria-expanded", "true");
 }
 
 // close hamburger menu icon mobile behavior
 function closeMenu() {
     menuNav.classList.remove("is-open");
-    hamburgerMenu.setAttribute("aria-expanded", "false");
+    headerToggle.setAttribute("aria-expanded", "false");
 }
 
 // event listeners
-hamburgerMenu.addEventListener("click", openMenu);
+headerToggle.addEventListener("click", openMenu);
 btnCloseMenu.addEventListener("click", closeMenu);
