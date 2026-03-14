f78a6f20b575d6fd03245e8c9e27de53796a40d5
Felipe Souza <souza.felipe87@gmail.com>
2026-03-14 18:31:57 -0300

    add DOM selectores and async fetch function for destination data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 const headerToggle = document.querySelector(".header__toggle");
 const menuNav = document.querySelector(".main-nav");
 const btnCloseMenu = document.querySelector(".main-nav__close");
+const planetBtns = document.querySelectorAll(".planet-menu__btn");
+const planetImage = document.getElementById("planet-image");
+const planetName = document.getElementById("planet-name");
+const planetResume = document.getElementById("planet-resume");
+const planetDistance = document.getElementById("planet-distance");
+const planetTime = document.getElementById("planet-time");
+
+async function getDestinationData () {
+    try {
+        const response = await fetch("data.json");
+        const data = await response.json();
+        return data.destinations;
+    } catch (error) {
+        console.error("Error trying to catch intergalatic data:", error);
+    }
+}
 
 // hamburger menu icon mobile behavior
 function openMenu() {
